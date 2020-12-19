217f532ed33aa48d5e6f83fc00da81959fcc55af
hugsal <hugs.salazar@gmail.com>
2020-12-19 14:27:17 -0600

    Solution of third challenge

---
+++ problem_3.js
@@ -0,0 +1,7 @@
+const FizzBuzz = () => {
+    for(let i = 1; i <= 100; i++){
+        console.log(  i % 3 == 0 ? i % 5 == 0 ? 'FizzBuzz' : 'Fizz' : i % 5 === 0 ? 'Buzz': i )
+    } 
+}
+
+FizzBuzz();
